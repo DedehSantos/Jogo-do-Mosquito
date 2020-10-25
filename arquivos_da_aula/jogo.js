57ddf921c1a2b5f7fe819806ecc4f2f310765655
dedehsantos <dedehfrontend@gmail.com>
2020-10-25 13:53:04 -0300

    novo

--- arquivos_da_aula/jogo.js
+++ arquivos_da_aula/jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 10
 
 function ajustaTamanhoPalcoJogo(){
 
@@ -13,6 +14,20 @@ console.log(largura, altura)
 
 
 ajustaTamanhoPalcoJogo()
+var cronometro = setInterval(function(){
+
+	tempo -= 1
+	
+
+  if(tempo < 0){
+	 
+	  clearInterval(cronometro)
+	  clearInterval(criaMosquito)
+	  alert('vitoria')
+  } else {
+    document.getElementById('cronometro').innerHTML = tempo
+  }
+}, 1000)
 
 function posicaoRandomica(){
 	//remover o mosquito anterior (caso exista)
